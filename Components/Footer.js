01c19096e837154e9a0b792f90a7f009a256d2cb
iCarlosCarhuas <CRCT-2003@hotmail.com>
2023-02-17 22:14:49 -0500

    Digital Card
    Using React

---
+++ Components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer(){
+    return(
+    <div className ="footer-container">   
+        <i className="fab fa-twitter-square"></i>
+        <i className="fab fa-facebook-square"></i>
+        <i className="fab fa-instagram-square"></i>
+        <i className="fab fa-github-square"></i>
+    </div> 
+    )
+} 
---
+++ Components/Info.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Info(){
+    return(
+    <div className="info-container">
+        <nav>
+            <img className="photo-profile" src= "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width="320px"/>
+            <h1>Carlos Raúl</h1>
+            <h5>Frontend Developer</h5>
+            <p>C-Art&Studios.vercel.app</p>   
+        </nav>
+        
+        <button className="email-btn"><i className="fa-solid fa-envelope"></i> Email</button>
+        <button className="linkedin-btn"><i className="fa-brands fa-linkedin"></i> LinkedIn</button>
+    </div>
+    )
+} 
